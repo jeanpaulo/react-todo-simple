0878a20bb4972a615e926bb493706ac93d9d636a
Jean Paulo Amaral <jeanpaulo0@gmail.com>
2023-01-12 01:24:27 -0300

    Prettier e Eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  
   return (
     <div className="app">
-      <div className="header">
-
-      </div>
-
       <h1>Simple to-do</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
